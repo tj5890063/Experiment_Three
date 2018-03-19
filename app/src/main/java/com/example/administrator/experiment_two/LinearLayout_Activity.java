078d8eb437318a3588d90ebee931bf23af699fcb
Your Name <you@example.com>
2018-03-19 21:44:03 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/experiment_two/LinearLayout_Activity.java
@@ -0,0 +1,14 @@
+package com.example.administrator.experiment_two;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LinearLayout_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.linear_layout);
+    }
+}
+
