54478a0dd69564ed19a99b1444637e1d3d5168ed
Your Name <you@example.com>
2018-03-19 22:44:56 +0800

    Initial commit

--- app/src/main/java/com/example/administrator/experiment_two/LinearLayout_Activity.java
+++ app/src/main/java/com/example/administrator/experiment_two/LinearLayout_Activity.java
@@ -1,5 +1,6 @@
 package com.example.administrator.experiment_two;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -8,7 +9,7 @@ public class LinearLayout_Activity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.linear_layout);
+        setContentView(R.layout.table_layout);
     }
 }
 
